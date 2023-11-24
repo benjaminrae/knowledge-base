1455f9e979e4532f557761a2fe4c6ed72f1e85ed
Benjamin Rae <rae_b@egamingchain.io>
2023-11-24 13:52:14 +0100

    feat(knowledge-server.core): add login to user entity

--- knowledge-server/core/users/user.entity.ts
+++ knowledge-server/core/users/user.entity.ts
@@ -1,5 +1,6 @@
 import { AggregateRoot, UniqueEntityId } from '@knowledge-base/shared';
 import { UserCreatedEvent } from './events/user-created.event';
+import { UserLoggedInEvent } from './events/user-logged-in.event';
 import { MissingUserEmailException } from './exceptions/missing-user-email.exception';
 import { CreateUserProps, UserProps } from './types';
 
@@ -13,6 +14,18 @@ export class User extends AggregateRoot<UserProps> {
     user.addEvent(new UserCreatedEvent(user));
 
     return user;
+  }
+
+  public login(): void {
+    const now = new Date();
+
+    if (!this.props.firstLogin) {
+      this.props.firstLogin = now;
+    }
+
+    this.props.lastLogin = now;
+
+    this.addEvent(new UserLoggedInEvent(this));
   }
 
   protected validate(props: UserProps): void {
